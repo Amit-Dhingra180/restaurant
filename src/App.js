38ad32ba3d27dcf03062d1b44ce2cd63d632eb9c
Amit Dhingra <amitdhingra180@gmail.com>
2023-09-06 13:00:11 +0530

    uniquefood

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+
+
+
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path='/' exact Component={Home}/>
           <Route path='/menu' Component={Menu}/>
+          <Route path='/about' Component={About}/>
+          <Route path='/cart' Component={Cart}/>
+          <Route path='/contact' Component={Contact}/>
+          <Route path='/login' Component={Login}/>
         </Routes>
       </BrowserRouter>
     </div>
